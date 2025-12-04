41036c0d3cb5274c67ae36bcd7579bb87efc6a8f
juliacygong <jgong@g.hmc.edu>
2025-12-04 15:47:18 -0800

    working mcu files

---
+++ STM32L432KC_ADC.h
@@ -0,0 +1,16 @@
+#ifndef STM32L432KC_ADC_H
+#define STM32L432KC_ADC_H
+
+#include "STM32L432KC.h"
+
+#define ADC_PIN PB0  // Analog input pin
+
+void initADC(void);
+
+uint8_t readADC(void);
+
+void start_sampling(void);
+
+void stop_sampling(void);
+
+#endif
